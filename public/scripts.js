2aa532f54d6db01e815808e51e317dd97ad5bac2
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-02 10:52:35 -0600

    Append item to be packed to page on submit and post to backend

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,39 @@
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(e)
+const postItemDB = async name => {
+  try {
+    const idString = await fetch('api/v1/items', {
+      method: 'POST',
+      body: JSON.stringify({ name }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const id = await idString.json();
+    $('#item-input').val('');
+  } catch(error) {
+    console.log(error);
+  }
 }
 
-$('.add-item-form').on('click', handleSubmit)
+const appendItem = name => {
+  $('.packing-wrap').append(`
+    <article>
+      <h2>${name}</h2>
+      <label for='check-${name}'>Packed</label>
+      <input type='checkbox' id='check-${name}' />
+      <button class="delete">Delete</button>
+    </article>
+  `)
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const name = $('#item-input').val();
+  try {
+    postItemDB(name);
+    appendItem(name);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+$('.add-item-form').on('submit', handleSubmit)
